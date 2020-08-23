0eff5ea20e89e3b967f1a0fa66da727f2e91cec6
dhruv aggarwal <dhruv.1822it1051@kiet.edu>
2020-08-23 22:45:07 +0530

    first commit

---
+++ Level1/a2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,f=0;
+    string s;
+    getline(cin,s);
+    cout<<s[0];
+    for(i=1;i<s.length();i++)
+    {
+        if(f==1)
+            cout<<"."<<s[i];
+        if(s[i]==' ') f=1;
+        else f=0;
+    }
+    return 0;
+}
+
+ //   ALTER
+ //   """"""
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,f=0;
+    string s;
+    cin>>s;
+    cout<<s[0];
+    while(cin>>s)
+    {
+        cout<<"."<<s[0];
+    }
+    return 0;
+}
